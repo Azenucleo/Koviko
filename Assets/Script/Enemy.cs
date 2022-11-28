de87db312e6d8c71be8180980fbe93697526781d
Azenucleo <robin.morales@artfx.fr>
2022-11-28 10:07:43 +0100

    [add] new LD + new character/mob

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -1,65 +1,144 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Enemy : MonoBehaviour {
-    [Header("Movement")]
-    public float speed = 0.3f;
+    [Header("Stats")]
+    [SerializeField] float speed;
+    private float playerDetectTime;
+    public float playerDetectRate;
+    public float chaseRange;
+    bool lookRight;
+
     [Header("Attack")]
-
-    [SerializeField] private float attackDamage = 10f;
-    [SerializeField] private float attackSpeed = 1f;
+    [SerializeField] float attackRange;
+    [SerializeField] int damage;
+    [SerializeField] float attackRate;
+    private float lastAttackTime;
     private float canAttack;
+    public Transform attackPoint;
+    public LayerMask playerLayerMask;
 
-    [Header("Health")]
-    private float health;
 
-    private Transform target;
+    [Header("Component")]
+    Rigidbody2D rb;
+    private BasicMovement targetPlayer;
+    Animator anim;
 
-    [SerializeField] private float maxHealth;
 
-    private Transform Target;
+    [Header("PathFinding")]
+    public float nextWaypointDistance = 2f;
+    Path path;
+    int currentWayPoint = 0;
+    bool reachEndPath = false;
+    Seeker seeker;
 
-    private void Start () {
-        health = maxHealth;
+    void Start() {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        InvokeRepeating("UpdatePath",0f, 0.5f);
     }
 
-    public void TakeDamage(float dmg) {
-        health -= dmg;
-        Debug.Log("Enemy Health: " + health);
+    void OnPathComplete(Path p) {
+        if (!p.error) {
+            path = p;
+            currentWayPoint = 0;
+        }
+    }
 
-        if (health <=0) {
-            Destroy(gameObject);
+    void UpdatePath() {
+        if (seeker.IsDone() && targetPlayer != null) {
+            seeker.StartPath(rb.position, targetPlayer.transform.position, OnPathComplete);
+        }
+    }
+
+    private void Update() {
+        if(rb.velocity.x > 0 && lookRight || rb.velocity.x < 0 && !lookRight) {
+            Flip();
         }
     }
 
     private void FixedUpdate() {
-        if (target != null) {
-            float step = speed * Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        if(targetPlayer != null) {
+            float dist = Vector2.Distance(transform.position, targetPlayer.transform.position);
+
+        if(dist < attackRange && Time.time - lastAttackTime >= attackRate) {
+            lastAttackTime = Time.time;
+            anim.SetTrigger("attack");
+            rb.velocity = Vector2.zero;
+        } else if (dist > attackRange) {
+            if (path == null)
+                return;
+            
+            if (currentWayPoint >= path.vectorPath.Count) {
+                reachEndPath = true;
+            } else {
+                reachEndPath = false;
+            }
+
+            Vector2 direction = ((Vector2)path.vectorPath[currentWayPoint] - rb.position).normalized;
+            Vector2 force = direction * speed * Time.fixedDeltaTime;
+
+            rb.velocity = force;
+
+            float distance = Vector2.Distance(rb.position, path.vectorPath[currentWayPoint]);
+
+            if(distance < nextWaypointDistance) {
+                currentWayPoint++;
+            }
         }
+        else {
+            rb.velocity = Vector2.zero;
+        }
+        }
+        
+
+        DetectPlayer();
     }
 
-    private void OnCollisionStay2D(Collision2D other) {
-        if (other.gameObject.tag == "Player") {
-            if (attackSpeed <= canAttack) {
-                other.gameObject.GetComponent<PlayerHealth>().UpdateHealth(-attackDamage);
-                canAttack = 0f;
-            } else {
-                canAttack += Time.deltaTime;
+    void DetectPlayer() {
+        if(Time.time - playerDetectTime > playerDetectRate) {
+            playerDetectTime = Time.time;
+            foreach (BasicMovement player in FindObjectsOfType<BasicMovement>()) {
+                if(player != null) {
+                    float dist = Vector2.Distance(transform.position, player.transform.position);
+
+                    if(player == targetPlayer) {
+                        if(dist > chaseRange) {
+                            targetPlayer = null;
+                            rb.velocity = Vector2.zero;
+                            anim.SetBool("onMove", false);
+                        }
+                    }else if (dist < chaseRange) {
+                        if(targetPlayer == null)
+                            targetPlayer = player;
+                            anim.SetBool("onMove", true);
+                    }
+                }
             }
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Player") {
-            target = other.transform;
-        }
+    void Flip() {
+        lookRight = !lookRight;
+
+
+        transform.Rotate(0,180f, 0);
     }
 
-    private void OnTriggerExit2D(Collider2D other) {
-        if (other.gameObject.tag == "Player") {
-            target = null;
+    void Attack() {
+        Collider2D player = Physics2D.OverlapCircle(attackPoint.transform.position, 0.5f, playerLayerMask);
+
+        if(player.tag == "Player") {
+            if (attackRate <= canAttack) {
+                player.GetComponent<PlayerHealth>().UpdateHealth(-damage);
+                canAttack = 0f;
+            } else {
+                canAttack += Time.deltaTime;
+            }
         }
     }
 }
